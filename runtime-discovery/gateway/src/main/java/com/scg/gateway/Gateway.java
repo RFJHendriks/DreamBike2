bb5e70a9b02a3a97b489cc7368d05af1e6e10b94
Rick Hendriks <Rick Hendriks@host.docker.internal>
2020-06-27 15:39:38 +0200

    security?

--- runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
+++ runtime-discovery/gateway/src/main/java/com/scg/gateway/Gateway.java
@@ -2,7 +2,11 @@ package com.scg.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -22,4 +26,11 @@ public class Gateway {
 //			return true;
 //		}
 //	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory tokenRelay) {
+		return builder.routes().route("servlet-app",
+				r -> r.path("/profile", "/").filters(f -> f.filter(tokenRelay.apply())).uri("http://localhost:8000"))
+				.build();
+	}
 }
